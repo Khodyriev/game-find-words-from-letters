6072a2d3f5be2e27f21e147031b639065bea53ca
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-11-18 12:24:44 +0200

    Game construction in progress

---
+++ index.js
@@ -0,0 +1,53 @@
+let alphabet = [];
+
+for (let i = 0; i < 12; i++) {
+    alphabet.push('E');
+};
+
+for (let i = 0; i < 9; i++) {
+    alphabet.push('T');
+};
+
+for (let i = 0; i < 8; i++) {
+    alphabet.push('A');
+    alphabet.push('I');
+    alphabet.push('N');
+    alphabet.push('O');
+    alphabet.push('S');
+};
+
+for (let i = 0; i < 6; i++) {
+    alphabet.push('H');
+    alphabet.push('R');
+};
+
+for (let i = 0; i < 4; i++) {
+    alphabet.push('D');
+    alphabet.push('L');
+};
+
+for (let i = 0; i < 3; i++) {
+    alphabet.push('U');
+    alphabet.push('C');
+    alphabet.push('M');
+    alphabet.push('F');
+};
+
+for (let i = 0; i < 2; i++) {
+    alphabet.push('W');
+    alphabet.push('Y');
+    alphabet.push('G');
+    alphabet.push('P');
+    alphabet.push('B');
+};
+
+alphabet.push('V');
+alphabet.push('K');
+alphabet.push('Q');
+alphabet.push('J');
+alphabet.push('X');
+alphabet.push('Z');
+
+console.log(alphabet.length)
+
+
